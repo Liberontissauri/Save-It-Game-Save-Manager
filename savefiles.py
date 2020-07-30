de1037cd029b79fdb63dd16043a784b27c215294
Liberontissauri <jp.liberon@gmail.com>
2020-07-30 19:02:06 +0100

    Added the Savedata object to manage the group of savefiles

--- savefiles.py
+++ savefiles.py
@@ -1,6 +1,9 @@
 from os import mkdir, listdir
 from os.path import isdir, exists
+from shutil import copytree
 import json
+from zipfile import ZipFile
+import locate
 
 class Savefile():
     def __init__(self, name, path):
@@ -19,6 +22,40 @@ class Savefile():
 
         write_saveinfo(loaded_saveinfo)
 
+        if not exists(self.path):
+
+            reversed_path = ""
+
+            for char in self.path:
+                reversed_path = char + reversed_path
+            copytree(self.path, "./SaveData/"+self.path[-(reversed_path.find("/")):])
+
+class Savedata():
+    def __init__(self, path):
+        self.path = path
+        if ".zip" in self.path:
+            self.iscompressed = True
+        else:
+            self.iscompressed = False
+            
+    def compress(self):
+        if self.iscompressed == True:
+            raise Exception("Savedata is already compressed")
+
+        with ZipFile(self.path + "Savedata.zip","w") as zip:
+            for file in locate.get_paths_in_save(self.path):
+                zip.write(file)
+        
+        self.iscompressed = True
+
+    def decompress(self):
+        if self.iscompressed == False:
+            raise Exception("Savedata is already decompressed")
+
+        with ZipFile(self.path, "r") as zip:
+            zip.extractall()
+
+
 def read_saveinfo():
     with open("./SaveData/saveinfo.json", "r") as saveinfo:
         loaded_saveinfo = json.load(saveinfo)
@@ -27,6 +64,3 @@ def read_saveinfo():
 def write_saveinfo(data):
     with open("./SaveData/saveinfo.json", "w") as saveinfo:
         json.dump(data,saveinfo)
-
-
-    
