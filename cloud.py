53526a842ea1a5e020455ac0780327a7fc138bd0
Liberontissauri <jp.liberon@gmail.com>
2020-08-02 15:01:13 +0100

    Added cloud support (via dropbox)

---
+++ cloud.py
@@ -0,0 +1,39 @@
+import dropbox
+from dropbox import DropboxOAuth2FlowNoRedirect
+import savefiles
+from os import remove
+from os.path import exists
+
+class DataTransfer():
+    def __init__(self, secret):
+        self.secret = secret
+        self.dbx = dropbox.Dropbox(self.secret)
+    
+    def sendsaves(self):
+        to_send = savefiles.Savedata(".")
+        to_send.compress()
+
+        for entry in self.dbx.files_list_folder('').entries:
+            if entry.name == "Savedata.zip":
+                self.dbx.files_delete("/Savedata.zip")
+                break
+
+        with open("./Savedata.zip", "rb") as file:
+            self.dbx.files_upload(file.read(), "/Savedata.zip")
+
+        remove("./Savedata.zip")
+
+    def getsaves(self):
+        self.dbx.files_download_to_file("./Savedata.zip","/Savedata.zip")
+        savedata = savefiles.Savedata("./Savedata.zip")
+        savedata.decompress()
+
+def setupoauth2file(code):
+    auth_flow = DropboxOAuth2FlowNoRedirect("c02irh2snxzphs5", "p3fdzx55ae539q8")
+
+    auth_flow.start()
+
+    code = auth_flow.finish(code)
+
+    with open("./code.ini","w") as file:
+        file.write(code.access_token)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import filedialog
 import savefiles
+from webbrowser import open_new
+import cloud
 
 class App:
     def __init__(self, master):
@@ -43,7 +45,8 @@ class App:
         self.button_send = Button(self.export_and_import_frame, text = "Send Saves", command = self.send_files, width = 10)
         self.button_load = Button(self.export_and_import_frame, text = "Import Data", command = self.import_compressed_savedata, width = 10)
         self.button_export = Button(self.export_and_import_frame, text = "Export Data", command = self.export_savedata, width = 10)
-
+        self.button_send_to_cloud = Button(self.export_and_import_frame, text = "Send to cloud", command = self.send_files_to_cloud, width = 13)
+        self.button_get_from_cloud = Button(self.export_and_import_frame, text = "get from cloud", command = self.get_files_from_cloud, width = 13)
         # The widgets are created
 
 
@@ -66,7 +69,9 @@ class App:
 
         self.button_send.grid(row = 0, pady = (0, 10))
         self.button_load.grid(row = 1, pady = (0, 10))
-        self.button_export.grid(row = 2)
+        self.button_export.grid(row = 2, pady = (0, 10))
+        self.button_get_from_cloud.grid(row = 3, pady = (0, 10))
+        self.button_send_to_cloud.grid(row = 4)
 
         # Layout configured
 
@@ -82,6 +87,7 @@ class App:
         self.filemenu.add_command(label="Exit", command=master.destroy)
 
         self.configmenu = Menu(self.menubar, tearoff = 0)
+        self.configmenu.add_command(label="Configure dropbox", command=self.open_dropbox_window)
         self.menubar.add_cascade(label = "Configuration", menu=self.configmenu)
 
         self.menubar.add_command(label="Exit", command=master.destroy)
@@ -89,6 +95,7 @@ class App:
         # MenuBar Created
 
         self.add_save_window_is_open = False
+        self.dropbox_window_is_open = False
         self.last_active = None
 
         self.update_save_list()
@@ -184,11 +191,50 @@ class App:
         self.add_save_window_is_open = False
         self.add_save_window.destroy()
 
+    def open_dropbox_window(self):
+        def update_dropbox_config():
+            cloud.setupoauth2file(entry_code.get())
+
+        if self.dropbox_window_is_open == False:
+            self.dropbox_window = Toplevel(self.main_frame)
+            self.dropbox_window_is_open = True
+            self.dropbox_window.protocol("WM_DELETE_WINDOW",self.close_dropbox_window)
+
+            label_code = Label(self.dropbox_window, text = "Enter the code:")
+            entry_code = Entry(self.dropbox_window, width = 70)
+            button_update_cloud = Button(self.dropbox_window, command = update_dropbox_config, text = "Update")
+            label_instructions = Label(self.dropbox_window, text = "Copy the code given by the link below")
+            label_oauthlink = Label(self.dropbox_window, text = "https://www.dropbox.com/oauth2/authorize?response_type=code&client_id=c02irh2snxzphs5")
+            label_oauthlink.bind("<Button-1>", lambda e: open_new("https://www.dropbox.com/oauth2/authorize?response_type=code&client_id=c02irh2snxzphs5"))
+
+            label_code.grid(row = 0, column = 0, sticky = W, padx = (10,5), pady = (10,0))
+            entry_code.grid(row = 0, column = 1, pady = (10,0))
+            button_update_cloud.grid(row = 1, column = 0, sticky = W, padx = (10,5))
+            label_instructions.grid(row = 1, column = 1)
+            label_oauthlink.grid(row = 2, column = 0, columnspan = 2, padx = 10, pady = (5,10))
+
+    def close_dropbox_window(self):
+        self.dropbox_window_is_open = False
+        self.dropbox_window.destroy()
+
     def send_files(self):
         saveinfo = savefiles.read_saveinfo()
 
         for name in saveinfo:
             savefiles.copy_stored_save_to_game_save_location(name)
+
+    def send_files_to_cloud(self):
+        with open("./code.ini", "r") as file:
+            code = file.read()
+        cloud_transfer = cloud.DataTransfer(code)
+        cloud_transfer.sendsaves()
+
+    def get_files_from_cloud(self):
+        with open("./code.ini", "r") as file:
+            code = file.read()
+        cloud_transfer = cloud.DataTransfer(code)
+        cloud_transfer.getsaves()
+
 
 
 root = Tk()
