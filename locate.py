d9f93cddfae2ffd432c33a02b46bbbbb71b9cee6
Liberontissauri <jp.liberon@gmail.com>
2020-08-03 10:29:44 +0100

    removed get_path_from_file function from the locate module

--- locate.py
+++ locate.py
@@ -1,17 +1,6 @@
 from os import walk, getenv
 from os.path import join
 
-def get_paths_from_file():
-    pathlist = []
-    with open("./paths.ini", "r") as pathfile:
-        for line in pathfile:
-            if line[1] == ":":  # Detecting if a line is a path by checking if the second character is a colon (from C: ; D: ; etc)
-                if line[-1:] == "\n":
-                    pathlist.append(line[:-1])
-                else:
-                    pathlist.append(line)
-    return pathlist
-
 def create_path_file():
     with open("./paths.ini", "w") as path_file:
         path_file.write("[Paths]\n")
