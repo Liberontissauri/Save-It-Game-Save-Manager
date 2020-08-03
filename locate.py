42c4572651d6a6f22f237323507dda9fcb5b576f
Liberontissauri <jp.liberon@gmail.com>
2020-08-03 10:31:02 +0100

    removed create_path_file function since the paths.ini file was replaced by saveinfo.json

--- locate.py
+++ locate.py
@@ -1,10 +1,6 @@
 from os import walk, getenv
 from os.path import join
 
-def create_path_file():
-    with open("./paths.ini", "w") as path_file:
-        path_file.write("[Paths]\n")
-
 def get_paths_in_save(path_to_save): # Returns a list of paths to all the files in a save folder
     file_list = []
     for root, _, files in walk(path_to_save, topdown=False):
